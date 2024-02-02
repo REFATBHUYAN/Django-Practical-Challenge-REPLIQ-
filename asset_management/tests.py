abe04b988fa6a59b337a1fb2e23359ffbf2455da
REFATBHUYAN <refatbubt@gmail.com>
2024-02-02 23:14:09 +0600

    test code added

--- asset_management/tests.py
+++ asset_management/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Company, Employee, Device
 
-# Create your tests here.
+class CompanyModelTest(TestCase):
+    def test_company_str_method(self):
+        company = Company.objects.create(name='Test Company')
+        self.assertEqual(str(company), 'Test Company')
+
+class EmployeeModelTest(TestCase):
+    def test_employee_str_method(self):
+        user = User.objects.create_user(username='testuser', password='testpassword')
+        employee = Employee.objects.create(user=user, company=Company.objects.create(name='Test Company'))
+        self.assertEqual(str(employee), 'testuser')
+
+class DeviceModelTest(TestCase):
+    def test_device_str_method(self):
+        device = Device.objects.create(name='Test Device', company=Company.objects.create(name='Test Company'))
+        self.assertEqual(str(device), 'Test Device')
+
+
