78c929c736d419efc3da1c70800bb2a3db3961f6
REFATBHUYAN <refatbubt@gmail.com>
2024-02-02 23:52:12 +0600

    Automated API documentation added

--- asset_tracker/urls.py
+++ asset_tracker/urls.py
@@ -16,8 +16,25 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Corporate Assets Tracker API",
+      default_version='v1',
+      description="Django app to track corporate assets such as phones, tablets, laptops and other gears handed out to employees.",
+      terms_of_service="http://127.0.0.1:8000/api/terms/",
+      contact=openapi.Contact(email="refatbubt@gmail.com"),
+      license=openapi.License(name="Tracker License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('asset_management.urls')),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
